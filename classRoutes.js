ee1481b159bd4e2cffd4ce6d6a42a4d577ce566f
Thokozani <tnmsezane@gmail.com>
2021-04-09 11:28:47 +0200

    Create functionality

--- classRoutes.js
+++ classRoutes.js
@@ -28,7 +28,9 @@ router.post('/edit', function (req, res) {
 })
 
 router.post('/api/create', function (req, res) {
-  console.log('creating a student entry')
+  console.log('creating a student entry', req.body.student)
+  classList.push(req.body.student)
+  res.redirect(req.baseUrl + '/api/list')
 })
 
 router.post('/api/delete', function (req, res) {
